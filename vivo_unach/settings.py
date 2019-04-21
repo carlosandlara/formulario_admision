941c41409914124a52a774773e3fe068b0c59eb4
Carlos Lara <carlos_laracu@hotmail.com>
2019-04-21 12:13:10 -0400

    121321

--- vivo_unach/settings.py
+++ vivo_unach/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'axz*7srf%)jw*-yvs!60++(i=dvrdocfa+w9375q)ip8sy_2x&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 """ALLOWED_HOSTS = ['127.0.0.1', 'carloslara.pythonanywhere.com']"""
 
